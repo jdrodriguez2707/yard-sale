b805c0e2f3bc957766b79dfaf8bf7c70e8f7cafd
jdrodriguez2707 <johandavid.jdr@gmail.com>
2024-05-20 16:30:28 -0500

    refactor: :recycle: Factor code to reduce lines and improve readability

--- main.js
+++ main.js
@@ -4,14 +4,15 @@ const navbarEmail = document.querySelector("#navbar-email");
 const desktopMenu = document.querySelector("#menu-desktop");
 const navbarIconExpand = document.querySelector("#navbar-icon-expand");
 
-barsIcon.addEventListener("click", toggleMobileMenu)
-navbarEmail.addEventListener("click", toggleDesktopMenu);
+barsIcon.addEventListener("click", () => {
+  toggleElementWithClass(mobileMenu, "inactive");
+});
 
-function toggleMobileMenu() {
-  mobileMenu.classList.toggle("inactive");
-}
+navbarEmail.addEventListener("click", () => {
+  toggleElementWithClass(desktopMenu, "inactive");
+  toggleElementWithClass(navbarIconExpand, "navbar__icon-expand--inverted");
+});
 
-function toggleDesktopMenu() {
-  desktopMenu.classList.toggle("inactive");
-  navbarIconExpand.classList.toggle("navbar__icon-expand--inverted");
+function toggleElementWithClass(element, className) {
+  element.classList.toggle(className);
 }
