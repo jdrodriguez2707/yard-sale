5bbf6da3376ecbcc01be0723c364c1e6c08c4f04
jdrodriguez2707 <johandavid.jdr@gmail.com>
2024-05-20 18:49:02 -0500

    refactor: :recycle: Factor code to reduce lines and improve readability
    I created the "closeIfIsOpened" function that hides elements if they are visible to reduce code lines

--- main.js
+++ main.js
@@ -1,55 +1,42 @@
-// Mobile
+// Select DOM elements
 const barsIcon = document.querySelector("#bars-icon");
 const mobileMenu = document.querySelector("#menu-mobile");
-
-// Mobile and desktop
 const shoppingCartIcon = document.querySelector("#shopping-cart-icon");
 const shoppingCartAside = document.querySelector("#shopping-cart-aside");
-
-// Desktop
 const navbarEmail = document.querySelector("#navbar-email");
 const desktopMenu = document.querySelector("#menu-desktop");
 const navbarIconExpand = document.querySelector("#navbar-icon-expand");
 
-
-// Mobile
+// Handle click on mobile menu icon
 barsIcon.addEventListener("click", () => {
-  const isShoppingCartAsideClosed = shoppingCartAside.classList.contains("inactive");
-
-  if (!isShoppingCartAsideClosed) {
-    shoppingCartAside.classList.add("inactive");
-  }
-
+  closeIfIsOpened(shoppingCartAside, "inactive");
   toggleElementWithClass(mobileMenu, "inactive");
 });
 
-// Mobile and desktop
+// Handle click on shopping cart icon
 shoppingCartIcon.addEventListener("click", () => {
-  const isMobileMenuClosed = mobileMenu.classList.contains("inactive");
-  const isDesktopMenuClosed = desktopMenu.classList.contains("inactive");
-
-  if (!isMobileMenuClosed) {
-    mobileMenu.classList.add("inactive");
-  } else if (!isDesktopMenuClosed) {
-    desktopMenu.classList.add("inactive");
-    navbarIconExpand.classList.remove("navbar__icon-expand--inverted");
-  }
-
+  closeIfIsOpened(mobileMenu, "inactive");
+  closeIfIsOpened(desktopMenu, "inactive");
+  navbarIconExpand.classList.remove("navbar__icon-expand--inverted");
   toggleElementWithClass(shoppingCartAside, "inactive");
 });
 
-// Desktop
+// Handle click on navbar email
 navbarEmail.addEventListener("click", () => {
-  const isShoppingCartAsideClosed = shoppingCartAside.classList.contains("inactive");
-
-  if (!isShoppingCartAsideClosed) {
-    shoppingCartAside.classList.add("inactive");
-  }
-
+  closeIfIsOpened(shoppingCartAside, "inactive");
   toggleElementWithClass(desktopMenu, "inactive");
   toggleElementWithClass(navbarIconExpand, "navbar__icon-expand--inverted");
 });
 
+// To toggle class on an element
 function toggleElementWithClass(element, className) {
   element.classList.toggle(className);
+}
+
+// To close or hide element if it is open
+function closeIfIsOpened(element, className) {
+  const isElementClosed = element.classList.contains(className);
+  if (!isElementClosed) {
+    element.classList.add(className); // Hide element by adding the 'inactive' class
+  }
 }
